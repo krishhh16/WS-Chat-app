f887367e512b9a2b798481c16f8dcf139ab485e4
Zeref <krishh16@yahoo.com>
2024-05-12 11:15:08 +0530

    added something

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,1 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
-
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  // options
-});
-
-io.on("connection", (socket) => {
-  // ...
-});
-
-httpServer.listen(3000, () => {
-    console.log('WS server started with socket io')
-});
+console.log('Hello world')
