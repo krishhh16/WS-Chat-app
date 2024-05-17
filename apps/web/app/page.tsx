f8b1f1130e25628bd72c93b11df15c03a7de452b
Zeref <krishh16@yahoo.com>
2024-05-17 09:32:04 +0530

    added frontend feature for sidebar

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,48 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
+
+interface MsgType {
+  username: string;
+  message: string;
+  selfEnd: boolean
+}
 
 const GoingChat = () => {
-  const arr = [
-    {username: "John", message: 'What the fuck??', selfEnd: false},
-    {username: "John", message: 'What the fuck??', selfEnd: false},
-    {username: "Me", message: 'Nothing', selfEnd: true},
-  ]
+  
+  const [msgs, setMsgs] = useState<MsgType[]>([]);
+  
+  const [text, setText] = useState<string>("");
+  const arr = msgs
   .map((item) => {
     return (
       <Message username={item.username} text={item.message} selfEnd={item.selfEnd} />
     )
   })
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-lg border rounded-lg overflow-hidden shadow-lg">
-        <div className="bg-blue-500 text-white p-3 text-center">Welcome to the group!</div>
-        <div className="p-3 max-h-96 overflow-y-auto">
+    <div className="flex">
+      {/* this is the sidebar */}
+    <div className="w-1/3 border-r-4">
+
+    </div>  
+    {/* This is the chat window section */}
+    <div className="flex w-2/3 justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-4/5 h-screen border rounded-lg overflow-hidden shadow-lg">
+        <div className="bg-blue-500 text-white p-3 text-center">Chat with Dave!</div>
+        <div className="p-3 max-h-96 overflow-y-auto min-h-96 ">
           {arr}
         </div>
-        <div className="p-3 border-t">
-          <input className="w-full px-4 py-2 rounded-lg border outline-none focus:border-blue-500" type="text" placeholder="Type your message here..." />
+        <div className="p-3 border-t ">
+          <input onChange={(e) => {setText(e.target.value)}} className="w-full px-4 py-2 rounded-lg border outline-none focus:border-blue-500" value={text} type="text" placeholder="Type your message here..." />
+          <button className="w-2/3 mx-auto justify-center rounded-xl bg-white font-semibold  text-blue-500" onClick={() => {
+            setMsgs([...msgs, {username: "Me", message: text , selfEnd: true}]); 
+            console.log('Set msgs completed')
+            setText('')
+            console.log('setText complete')
+            }} type="submit">Send message</button>
         </div>
       </div>
+    </div>
     </div>
   );
 };
