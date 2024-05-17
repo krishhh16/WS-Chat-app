79f80d25a8662518e7421fc25ae0572b7798b59e
Zeref <krishh16@yahoo.com>
2024-05-17 10:47:36 +0530

    added signup route

---
+++ apps/server/prisma/migrations/20240517050843_something/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userID" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userID")
+);
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,8 +2,11 @@ import express from "express";
 import {Server} from "socket.io";
 import {createServer} from "http";
 import cors from "cors"
+import { PrismaClient } from "@prisma/client";
+
 
 const port = 3001;
+const prisma = new PrismaClient();
 const app = express();
 const server = createServer(app)
 const io = new Server(server, {
@@ -22,12 +25,39 @@ app.use(
   })
 )
 
+app.get("/", (req, res) =>{
+  res.json({msg: "Hello world"})
+})
+
+app.post('/signup', async (req, res) => {
+  const {username} = await req.body;
+  if(!username){
+    return res.status(404).json({success: false})
+  }
+
+  try{
+    await prisma.user.create({
+      data: {
+        username
+      }
+    })
+
+    return res.json({success: true})
+  }catch(err){
+    console.log(err);
+    return res.json({success: false})
+  }
+
+})
+
+
+
 io.on("connection", (socket) => {
   console.log(`User connected with id ${socket.id} `);
 
   socket.on("message", ({room, msg}) =>{
     console.log({room, msg})
-    socket.broadcast.emit('received', {msg})
+    io.emit('received', {msg})
     console.log('emitted the message')
   })
 
