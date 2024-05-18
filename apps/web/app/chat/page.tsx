1c92f6ff0a668e1aee743d1826414d2e52213a02
Zeref <krishh16@yahoo.com>
2024-05-18 11:26:10 +0530

    updated the schema

--- apps/web/app/chat/page.tsx
+++ apps/web/app/chat/page.tsx
@@ -4,17 +4,10 @@ import React from 'react';
 import Sidebar from '../../components/Sidebar';
 import ChatComponent from '../../components/ChatComponent';
 
-interface MsgType {
-  username: string;
-  message: string;
-  selfEnd: boolean
-}
-
 const GoingChat = () => {
   
   return (
     <div className="flex">
-      {/* this is the sidebar */}
     <Sidebar/>
     <ChatComponent/> 
     </div>
